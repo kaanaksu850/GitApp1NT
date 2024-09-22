1fbd2062954766e86369875bd0c5c98742fe767c
Kaan Aksu <kaanaksu850@gmail.com>
2024-09-22 10:55:54 +0300

    v1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,9 +1,8 @@
 public class Runner {
     public static void main(String[] args) {
 
-        System.out.println("versiyon 1");
-        System.out.println("versiyon 2");
-        System.out.println("versiyon 2nin devamı");
+        System.out.println("merhaba git");
+
 
     }
 }
