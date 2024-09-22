7a8d1b41d42e8e4cfe7fef7e3a938690627e7ed6
Kaan Aksu <kaanaksu850@gmail.com>
2024-09-22 11:01:55 +0300

    v2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,6 +2,8 @@ public class Runner {
     public static void main(String[] args) {
 
         System.out.println("merhaba git");
+        System.out.println("merhaba git1");
+        System.out.println("merhaba git2nin devami");
 
 
     }
