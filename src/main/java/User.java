e4c0525ec7d16d54a39fa8b44f225b8e260fc93b
Kaan Aksu <kaanaksu850@gmail.com>
2024-09-28 11:45:17 +0300

    v3

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,6 +1,9 @@
 public class User {
     public static void main(String[] args) {
+        System.out.println("versiyon 1");
+        System.out.println("versiyon 2");
         System.out.println("versiyon 3");
+        System.out.println("versiyon 4 devamı");
 
 
 
